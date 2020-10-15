65b8bd0c34750818667612912db93fb6ea72126b
willys_tjiawi <willys@huubap.com>
2020-10-15 17:50:47 +0800

    commented demo example to avoid error

--- lib/main-bloc.dart
+++ lib/main-bloc.dart
@@ -1,43 +1,43 @@
-import 'package:bloc/bloc.dart';
+// import 'package:bloc/bloc.dart';
 
-enum CounterEvent { INCREMENT, DECREMENT }
+// enum CounterEvent { INCREMENT, DECREMENT }
 
-class CounterBloc extends Bloc<CounterEvent, int> {
-  CounterBloc() : super(0);
+// class CounterBloc extends Bloc<CounterEvent, int> {
+//   CounterBloc() : super(0);
 
-  // https://stackoverflow.com/questions/55397023/whats-the-difference-between-async-and-async-in-dart#:~:text=2%20Answers&text=Marking%20a%20function%20as%20async,a%20value%20through%20yield%20keyword.
-  // https://dart.dev/guides/language/language-tour#generators
-  @override
-  Stream<int> mapEventToState(CounterEvent event) async* {
-    switch (event) {
-      case CounterEvent.INCREMENT:
-        yield state + 1;
-        break;
-      case CounterEvent.DECREMENT:
-        yield state - 1;
-        break;
-      default:
-        yield state;
-    }
-  }
-}
+//   // https://stackoverflow.com/questions/55397023/whats-the-difference-between-async-and-async-in-dart#:~:text=2%20Answers&text=Marking%20a%20function%20as%20async,a%20value%20through%20yield%20keyword.
+//   // https://dart.dev/guides/language/language-tour#generators
+//   @override
+//   Stream<int> mapEventToState(CounterEvent event) async* {
+//     switch (event) {
+//       case CounterEvent.INCREMENT:
+//         yield state + 1;
+//         break;
+//       case CounterEvent.DECREMENT:
+//         yield state - 1;
+//         break;
+//       default:
+//         yield state;
+//     }
+//   }
+// }
 
-main(List<String> args) async {
-  final counterBloc = CounterBloc();
+// main(List<String> args) async {
+//   final counterBloc = CounterBloc();
 
-  // demonstrate bloc is a Stream with
-  // EVENT as trigger & output the state
+//   // demonstrate bloc is a Stream with
+//   // EVENT as trigger & output the state
 
-  final counterBlocSub = counterBloc.listen(print);
+//   final counterBlocSub = counterBloc.listen(print);
 
-  counterBloc.add(CounterEvent.INCREMENT);
-  counterBloc.add(CounterEvent.DECREMENT);
-  counterBloc.add(CounterEvent.INCREMENT);
-  counterBloc.add(CounterEvent.INCREMENT);
-  counterBloc.add(CounterEvent.INCREMENT);
+//   counterBloc.add(CounterEvent.INCREMENT);
+//   counterBloc.add(CounterEvent.DECREMENT);
+//   counterBloc.add(CounterEvent.INCREMENT);
+//   counterBloc.add(CounterEvent.INCREMENT);
+//   counterBloc.add(CounterEvent.INCREMENT);
 
-  await Future.delayed(Duration.zero);
+//   await Future.delayed(Duration.zero);
 
-  await counterBlocSub.cancel();
-  await counterBloc.close();
-}
+//   await counterBlocSub.cancel();
+//   await counterBloc.close();
+// }
--- lib/main-cubit.dart
+++ lib/main-cubit.dart
@@ -1,58 +1,58 @@
-import 'package:bloc/bloc.dart';
+// import 'package:bloc/bloc.dart';
 
-class CounterCubit extends Cubit<int> {
-  CounterCubit() : super(0);
+// class CounterCubit extends Cubit<int> {
+//   CounterCubit() : super(0);
 
-  void increment() => emit(state + 1);
-  void decrement() => emit(state - 1);
-}
+//   void increment() => emit(state + 1);
+//   void decrement() => emit(state - 1);
+// }
 
-void main(List<String> args) async {
-  final counterCubit = CounterCubit();
+// void main(List<String> args) async {
+//   final counterCubit = CounterCubit();
 
-  // demonstrate cubit is a Stream with
-  // FUNCTION as trigger & output the state
+//   // demonstrate cubit is a Stream with
+//   // FUNCTION as trigger & output the state
 
-  // // initial state value: 0
-  // print(counterCubit.state);
+//   // // initial state value: 0
+//   // print(counterCubit.state);
 
-  // // incremental
-  // counterCubit.increment();
-  // print(counterCubit.state);
+//   // // incremental
+//   // counterCubit.increment();
+//   // print(counterCubit.state);
 
-  // counterCubit.increment();
-  // print(counterCubit.state);
+//   // counterCubit.increment();
+//   // print(counterCubit.state);
 
-  // counterCubit.increment();
-  // print(counterCubit.state);
+//   // counterCubit.increment();
+//   // print(counterCubit.state);
 
-  // // decremental
-  // counterCubit.decrement();
-  // print(counterCubit.state);
+//   // // decremental
+//   // counterCubit.decrement();
+//   // print(counterCubit.state);
 
-  // counterCubit.decrement();
-  // print(counterCubit.state);
+//   // counterCubit.decrement();
+//   // print(counterCubit.state);
 
-  // counterCubit.decrement();
-  // print(counterCubit.state);
+//   // counterCubit.decrement();
+//   // print(counterCubit.state);
 
-  // since cubit is a Stream, we can listen by subscribing to it
-  final counterCubitSub = counterCubit.listen(print);
+//   // since cubit is a Stream, we can listen by subscribing to it
+//   final counterCubitSub = counterCubit.listen(print);
 
-  counterCubit.increment();
-  counterCubit.decrement();
-  counterCubit.increment();
-  counterCubit.increment();
-  counterCubit.increment();
+//   counterCubit.increment();
+//   counterCubit.decrement();
+//   counterCubit.increment();
+//   counterCubit.increment();
+//   counterCubit.increment();
 
-  // this is because we do not want to cancel the subscription immediately
-  // below code
-  await Future.delayed(Duration.zero);
+//   // this is because we do not want to cancel the subscription immediately
+//   // below code
+//   await Future.delayed(Duration.zero);
 
-  // since it is Stream & subscription of Stream,
-  // we need to cancel/close it to avoid memory leak
-  // for flutter_bloc, flutter_bloc handles the closing of
-  // subscriptions & stream automatically for us
-  await counterCubitSub.cancel();
-  await counterCubit.close();
-}
+//   // since it is Stream & subscription of Stream,
+//   // we need to cancel/close it to avoid memory leak
+//   // for flutter_bloc, flutter_bloc handles the closing of
+//   // subscriptions & stream automatically for us
+//   await counterCubitSub.cancel();
+//   await counterCubit.close();
+// }
