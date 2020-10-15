b1c858282087582d6346979a184cfcbb54d1a908
willys_tjiawi <willys@huubap.com>
2020-10-15 16:55:23 +0800

    demo: Cubit, add subscription to listen to cubit stream

--- lib/main-cubit.dart
+++ lib/main-cubit.dart
@@ -7,32 +7,52 @@ class CounterCubit extends Cubit<int> {
   void decrement() => emit(state - 1);
 }
 
-void main(List<String> args) {
+void main(List<String> args) async {
   final counterCubit = CounterCubit();
 
   // demonstrate cubit is a Stream with
   // FUNCTION as trigger & output the state
 
-  // initial state value: 0
-  print(counterCubit.state);
+  // // initial state value: 0
+  // print(counterCubit.state);
 
-  // incremental
+  // // incremental
+  // counterCubit.increment();
+  // print(counterCubit.state);
+
+  // counterCubit.increment();
+  // print(counterCubit.state);
+
+  // counterCubit.increment();
+  // print(counterCubit.state);
+
+  // // decremental
+  // counterCubit.decrement();
+  // print(counterCubit.state);
+
+  // counterCubit.decrement();
+  // print(counterCubit.state);
+
+  // counterCubit.decrement();
+  // print(counterCubit.state);
+
+  // since cubit is a Stream, we can listen by subscribing to it
+  final counterCubitSub = counterCubit.listen(print);
+
   counterCubit.increment();
-  print(counterCubit.state);
-
+  counterCubit.decrement();
   counterCubit.increment();
-  print(counterCubit.state);
-
   counterCubit.increment();
-  print(counterCubit.state);
+  counterCubit.increment();
 
-  // decremental
-  counterCubit.decrement();
-  print(counterCubit.state);
+  // this is because we do not want to cancel the subscription immediately
+  // below code
+  await Future.delayed(Duration.zero);
 
-  counterCubit.decrement();
-  print(counterCubit.state);
-
-  counterCubit.decrement();
-  print(counterCubit.state);
+  // since it is Stream & subscription of Stream,
+  // we need to cancel/close it to avoid memory leak
+  // for flutter_bloc, flutter_bloc handles the closing of
+  // subscriptions & stream automatically for us
+  await counterCubitSub.cancel();
+  await counterCubit.close();
 }
